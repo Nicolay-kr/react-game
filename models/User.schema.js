da9039f0a3569478a092fb186495bf68cc8efd61
Nicolay-kr <nicolay.krischenovich@gmail.com>
2021-04-30 20:30:58 +0300

    build

--- models/User.schema.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model, Types } = require('mongoose');
-
-const schema = new Schema({
-	name: { type: String },
-	email: { type: String, required: true, unique: true },
-	password: { type: String, required: true },
-});
-
-module.exports = model('User', schema);
